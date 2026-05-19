b7869680138e8f6ece3fe793dd5d0e40ea0d6b14
chjh1211 <chjh8800@naver.com>
2026-05-19 19:19:55 +0900

    init: spring boot 초기 설정

---
+++ src/main/java/wearweather/wearweather_server/WearweatherServerApplication.java
@@ -0,0 +1,13 @@
+package wearweather.wearweather_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearweatherServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearweatherServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wearweather/wearweather_server/WearweatherServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wearweather.wearweather_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearweatherServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
